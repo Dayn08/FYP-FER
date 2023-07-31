3de41a74aa750f6c982f5c363211edbcecde1170
Dayn08 <nizamuddin.rosli@gmail.com>
2023-07-31 18:57:00 +0800

    updated

--- app.py
+++ app.py
@@ -144,5 +144,5 @@ def emotion_data():
     return Response(generate(), content_type='text/event-stream')
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+    app.run(debug=True)
  
